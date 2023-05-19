8912ca1648275bd95dbdcf56e8a36180ae6863aa
nicovillamonte <nicovillamonte@gmail.com>
2023-05-19 02:28:30 -0300

    TERMINADO FUNCIONANDO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,60 +17,60 @@ import { Data } from './DTO/data.dto';
 })
 export class AppComponent {
   title = 'happiness-ai';
-  @ViewChild('stepper') stepper!: MatStepper;
-  @ViewChildren('step') steps!: QueryList<any>;
+  // @ViewChild('stepper') stepper!: MatStepper;
+  // @ViewChildren('step') steps!: QueryList<any>;
 
-  probando!: number;
-  continueButtonText: string = 'Continuar';
-  lastStep!: boolean;
+  // probando!: number;
+  // continueButtonText: string = 'Continuar';
+  // lastStep!: boolean;
 
-  data: Data = {
-    user: {},
-  };
+  // data: Data = {
+  //   user: {},
+  // };
 
-  firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
-  });
+  // firstFormGroup = this._formBuilder.group({
+  //   firstCtrl: ['', Validators.required],
+  // });
 
-  constructor(private _formBuilder: FormBuilder) {}
+  // constructor(private _formBuilder: FormBuilder) {}
 
-  onResultChange(newValue: any, type: string) {
-    switch (type) {
-      case 'user':
-        this.data.user = newValue;
-        break;
-      case 'mentalHealth':
-        this.data.mentalHealth = newValue;
-        break;
-      case 'emotionalHealth':
-        this.data.emotionalHealth = newValue;
-        break;
-      case 'socialRelation':
-        this.data.socialRelation = resultToSocialRelationType(newValue);
-        break;
-      case 'familyRelation':
-        this.data.familyRelation = newValue;
-        break;
-      case 'purposeSense':
-        this.data.purposeSense = resultToPurposeSenseType(newValue);
-        break;
-    }
-  }
+  // onResultChange(newValue: any, type: string) {
+  //   switch (type) {
+  //     case 'user':
+  //       this.data.user = newValue;
+  //       break;
+  //     case 'mentalHealth':
+  //       this.data.mentalHealth = newValue;
+  //       break;
+  //     case 'emotionalHealth':
+  //       this.data.emotionalHealth = newValue;
+  //       break;
+  //     case 'socialRelation':
+  //       this.data.socialRelation = resultToSocialRelationType(newValue);
+  //       break;
+  //     case 'familyRelation':
+  //       this.data.familyRelation = newValue;
+  //       break;
+  //     case 'purposeSense':
+  //       this.data.purposeSense = resultToPurposeSenseType(newValue);
+  //       break;
+  //   }
+  // }
 
-  isLastStep() {
-    return this.lastStep;
-  }
+  // isLastStep() {
+  //   return this.lastStep;
+  // }
 
-  finish() {
-    console.log('Finalizado: ', this.data);
+  // finish() {
+  //   console.log('Finalizado: ', this.data);
 
-    let total = getTotalResult(this.data);
-    console.log('Total: ', total);
-  }
+  //   let total = getTotalResult(this.data);
+  //   console.log('Total: ', total);
+  // }
 
-  textContinueButton(event: any) {
-    const isLastStep = event.selectedIndex === this.stepper.steps.length - 1;
-    this.continueButtonText = isLastStep ? 'Finalizar' : 'Continuar';
-    this.lastStep = isLastStep ? true : false;
-  }
+  // textContinueButton(event: any) {
+  //   const isLastStep = event.selectedIndex === this.stepper.steps.length - 1;
+  //   this.continueButtonText = isLastStep ? 'Finalizar' : 'Continuar';
+  //   this.lastStep = isLastStep ? true : false;
+  // }
 }
